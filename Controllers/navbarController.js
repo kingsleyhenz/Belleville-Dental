df545f4eb9426083582198f7fbe0619e545618c7
BLACKHENZ29 <ayehenz29@gmail.com>
2023-04-13 15:49:55 +0100

    Complete

---
+++ Controllers/navbarController.js
@@ -0,0 +1,12 @@
+function toggleDropdown() {
+    console.log("here");
+    const dropdownMenu = document.querySelector('.drpdwn');
+    dropdownMenu.classList.toggle('show');
+  }
+  
+function toggleTabs() {
+    console.log('here');
+    const mainNav = document.querySelector('.main-nav');
+    mainNav.classList.toggle('showing');
+}
+  
